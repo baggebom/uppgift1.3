97ec0fd31d292113f6821c42c4e7ca03a99cf1b4
Admin <Admin@TEB123004>
2023-09-08 14:58:43 +0200

    Add project files.

---
+++ uppgift 1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Uppgift1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("  Oskar Nilsson har gjort denna");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+           Console.WriteLine(" / ,, \\");
+          Console.WriteLine("/______\\");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
